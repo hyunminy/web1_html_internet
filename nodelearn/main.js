9cbfc3c3f73f455efefc914b075514a7784a4688
김현민 <hyunminy@gimhyeonmin-ui-MacBookAir.local>
2020-07-11 17:52:11 +0900

    main.js url 입력되 값 사용 해결

--- nodelearn/main.js
+++ nodelearn/main.js
@@ -13,6 +13,6 @@ var app = http.createServer(function (request, response) {
     return response.writeHead(404);
   }
   response.writeHead(200);
-  response.end(fs.readFileSync(__dirname + _url));
+  response.end(queryData.id);
 });
 app.listen(3000);
