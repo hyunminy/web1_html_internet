960c5ab8319d583799e19b27c30e412f88fed66c
김현민 <hyunminy@gimhyeonmin-ui-MacBookAir.local>
2020-07-13 02:11:31 +0900

    .

--- syntax/template.js
+++ syntax/template.js
@@ -2,12 +2,12 @@ var name = "KYB";
 var letter =
   "Dear " +
   name +
-  " asljdkflknwe jadl;afk nlwkaejf alskdjf nakdjf lwkf lakdj fklqwlk fldnfk wlbf akdsjflkwnlfd k mslfk nsdlkwnq " +
+  "Amet in dolore exercitation duis duis officia ipsum." +
   name +
   " lknf ";
-"lakdnfl kn KHM lkwn flksjaoicujaoskdflknwlk " +
+"Nulla exercitation adipisicing non cupidatat ad eu nisi incididunt amet labore elit sint. " +
   name +
-  " dmcla knl knaslkdn lakmscl kndlckansl kdncl knalknc lksmclk awnlknc asijcoa inw ock nwalkf";
+  "Ipsum elit culpa et velit enim. Nulla ut enim Lorem laboris adipisicing ex aliquip non elit nostrud. Ex occaecat eiusmod in sit minim incididunt minim id commodo. Proident aliquip ad laboris proident aliqua voluptate ex deserunt. Culpa esse ad commodo mollit. Dolor cillum reprehenderit fugiat veniam mollit ipsum et laborum. Incididunt reprehenderit nulla in ullamco nisi culpa amet aliqua irure et qui sit ea est.";
 
 var letter = `Dear  ${name}
 Sunt proident qui magna deserunt ${name} id occaecat nulla qui ${name} elit. Consequat elit voluptate duis consequat ${name} aute duis id KYB cillum excepteur excepteur. Fugiat commodo laboris velit nisi ex dolor adipisicing duis nostrud. Ipsum ${name} do aute ad ea commodo ${name} nisi aute adipisicing. Voluptate adipisicing adipisicing ut non ex incididunt duis do.`;
