4fdee4e6f078340cc4ef7b8107d7a81de50efb50
김현민 <hyunminy@gimhyeonmin-ui-MacBookAir.local>
2020-07-13 01:25:20 +0900

    비교연산자

---
+++ comparison.js
@@ -0,0 +1,10 @@
+console.log(1 + 1);
+console.log(1 == 1); //true
+console.log(1 == 2); //false
+console.log(1 > 2); //false
+console.log(1 < 2); //true
+console.log(1 === 1); //true
+console.log(1 === 2); //false
+//name == 1;
+
+//"=="은 맞는 것인가? "==="는 정확히 맞는 것인가?
