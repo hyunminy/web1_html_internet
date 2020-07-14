8c92fd06895560d2a6e1ba891aeb1e9e25c1a21d
김현민 <hyunminy@gimhyeonmin-ui-MacBookAir.local>
2020-07-15 02:56:31 +0900

    javascript 함수의 입력

---
+++ syntax/function2.js
@@ -0,0 +1,9 @@
+//round=반올림
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+
+function sum(first, second) {
+  //매개변수(parameter)
+  console.log(first + second);
+}
+sum(2, 4); // argument
