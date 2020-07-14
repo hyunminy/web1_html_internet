bdc7c3eeb4207c2c3af3f1d516fb4a56189ea23e
김현민 <hyunminy@gimhyeonmin-ui-MacBookAir.local>
2020-07-15 01:20:53 +0900

    data 파일에 목록을 추가해서 다시 페이지를 실행 시켰을 때, 자동으로 페이지에 추가됨

--- nodelearn/main.js
+++ nodelearn/main.js
@@ -11,15 +11,9 @@ var app = http.createServer(function (request, response) {
   if (pathname === "/") {
     if (queryData.id === undefined) {
       fs.readdir("./data", function (error, filelist) {
-        console.log(filelist);
         var title = "welcome";
         var description = "Hello, Node.js";
-        //   var list=`
-        //   <ul>
-        //   <li><a href="/?id=HTML">HTML</a></li>
-        //   <li><a href="/?id=CSS">CSS</a></li>
-        //   <li><a href="/?id=javaScript">JavaScript</a></li>
-        // </ul>`
+
         var list = "<ul>";
 
         var i = 0;
@@ -50,6 +44,17 @@ var app = http.createServer(function (request, response) {
         response.end(template);
       });
     } else {
+      var title = "welcome";
+      var description = "Hello, Node.js";
+      var list = "<ul>";
+      var i = 0;
+      while (i < filelist.length) {
+        list =
+          list + `<li><a href="/?id${filelist[i]}">${filelist[i]}</a></li>`;
+        i = i + 1;
+      }
+
+      list = list + "</ul>";
       fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
         var title = queryData.id;
         var template = `
@@ -61,11 +66,7 @@ var app = http.createServer(function (request, response) {
     </head>
     <body>
       <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=javaScript">JavaScript</a></li>
-      </ul>
+      ${list}
       <h2>${title}</h2>
       <p>${description}
       </p>
