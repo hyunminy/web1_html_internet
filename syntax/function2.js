ad288829ce303844d67aabd0dd972d481ffc4c18
김현민 <hyunminy@gimhyeonmin-ui-MacBookAir.local>
2020-07-15 03:08:43 +0900

    함수의 출력(함수의 마지막 이야기)

--- syntax/function2.js
+++ syntax/function2.js
@@ -3,7 +3,7 @@ console.log(Math.round(1.6));
 console.log(Math.round(1.4));
 
 function sum(first, second) {
-  //매개변수(parameter)
-  console.log(first + second);
+  console.log("a");
+  return first + second;
 }
-sum(2, 4); // argument
+console.log(sum(2, 4)); // argument
