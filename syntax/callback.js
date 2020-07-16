c1e158b44e49c852ab6b2e0144f759363e0e3a58
김현민 <hyunminy@gimhyeonmin-ui-MacBookAir.local>
2020-07-16 15:23:49 +0900

    callback의 형식

--- syntax/callback.js
+++ syntax/callback.js
@@ -1,29 +1,12 @@
+// function a() {
+//   console.log("A");
+// }
+
 var a = function () {
-  console.log("A강아지를 찾습니다.");
+  console.log("A");
 };
 
-var b = function () {
-  console.log("B강아지를 찾습니다");
-};
-
-// 쉬운 방법
 function slowfunc(callback) {
   callback();
 }
-// 어려운 방법
-function hardShowfunc(text) {
-  if (text == "a") {
-    a();
-  } else if (text == "b") {
-    B();
-  } else {
-    console.log("에러");
-  }
-}
-hardShowfunc("a");
-
 slowfunc(a);
-// 직접작성
-slowfunc(() => {
-  console.log("강아지");
-});
