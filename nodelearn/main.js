11dc244a7f5fdbe1882846c5d5a2b9fc34e04e08
김현민 <hyunminy@gimhyeonmin-ui-MacBookAir.local>
2020-07-16 12:00:40 +0900

    main.js 7시간만에 오류 해결..

--- nodelearn/main.js
+++ nodelearn/main.js
@@ -2,6 +2,35 @@ var http = require("http");
 var fs = require("fs");
 var url = require("url");
 
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist) {
+  var list = "<ul>";
+
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+
+  list = list + "</ul>";
+  return list;
+}
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -12,68 +41,31 @@ var app = http.createServer(function (request, response) {
       fs.readdir("./data", function (error, filelist) {
         var title = "welcome";
         var description = "Hello, Node.js";
-
-        var list = "<ul>";
-
-        var i = 0;
-        while (i < filelist.length) {
-          list =
-            list + `<li><a href="/?id${filelist[i]}">${filelist[i]}</a></li>`;
-          i = i + 1;
-        }
-
-        list = list + "</ul>";
-        var template = `
-      <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      <h2>${title}</h2>
-      <p>${description}
-      </p>
-    </body>
-    </html>
-      `;
+        var list = templateList(filelist);
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>${description}`
+        );
         response.writeHead(200);
         response.end(template);
       });
     } else {
-      var title = "welcome";
-      var description = "Hello, Node.js";
-      var list = "<ul>";
-      var i = 0;
-      while (i < filelist.length) {
-        list =
-          list + `<li><a href="/?id${filelist[i]}">${filelist[i]}</a></li>`;
-        i = i + 1;
-      }
-
-      list = list + "</ul>";
-      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-        var title = queryData.id;
-        var template = `
-      <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      <h2>${title}</h2>
-      <p>${description}
-      </p>
-    </body>
-    </html>
-      `;
-        response.writeHead(200);
-        response.end(template);
+      fs.readdir("./data", function (error, filelist) {
+        fs.readFile(`data/${queryData.id}`, "utf8", function (
+          err,
+          description
+        ) {
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2>${description}`
+          );
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
@@ -82,3 +74,77 @@ var app = http.createServer(function (request, response) {
   }
 });
 app.listen(3000);
+
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+
+// function templateHTML(title, list, body) {
+//   return `
+//   <!doctype html>
+//   <html>
+//   <head>
+//     <title>WEB1 - ${title}</title>
+//     <meta charset="utf-8">
+//   </head>
+//   <body>
+//     <h1><a href="/">WEB</a></h1>
+//     ${list}
+//     ${body}
+//   </body>
+//   </html>
+//   `;
+// }
+// function templateList(filelist) {
+//   var list = "<ul>";
+//   var i = 0;
+//   while (i < filelist.length) {
+//     list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+//     i = i + 1;
+//   }
+//   list = list + "</ul>";
+//   return list;
+// }
+
+// var app = http.createServer(function (request, response) {
+//   var _url = request.url;
+//   var queryData = url.parse(_url, true).query;
+//   var pathname = url.parse(_url, true).pathname;
+//   if (pathname === "/") {
+//     if (queryData.id === undefined) {
+//       fs.readdir("./data", function (error, filelist) {
+//         var title = "Welcome";
+//         var description = "Hello, Node.js";
+//         var list = templateList(filelist);
+//         var template = templateHTML(
+//           title,
+//           list,
+//           `<h2>${title}</h2>${description}`
+//         );
+//         response.writeHead(200);
+//         response.end(template);
+//       });
+//     } else {
+//       fs.readdir("./data", function (error, filelist) {
+//         fs.readFile(`data/${queryData.id}`, "utf8", function (
+//           err,
+//           description
+//         ) {
+//           var title = queryData.id;
+//           var list = templateList(filelist);
+//           var template = templateHTML(
+//             title,
+//             list,
+//             `<h2>${title}</h2>${description}`
+//           );
+//           response.writeHead(200);
+//           response.end(template);
+//         });
+//       });
+//     }
+//   } else {
+//     response.writeHead(404);
+//     response.end("Not found");
+//   }
+// });
+// app.listen(3000);
