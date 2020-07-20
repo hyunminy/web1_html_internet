059e25537554943508f84fd64f3adfa72d135ce0
김현민 <hyunminy@gimhyeonmin-ui-MacBookAir.local>
2020-07-21 00:50:49 +0900

    객체-반복문

--- syntax/Object.js
+++ syntax/Object.js
@@ -1,9 +1,18 @@
 var members = ["KHM", "KYB", "KSB"];
-console.log(members[1]);
+console.log(members[1]); //KYB
+var i = 0;
+while (i < members.length) {
+  console.log("array loop", members[i]);
+  i = i + 1;
+}
 
 var roles = {
   programmer: "KHM",
   designer: "KYB",
   manager: "KSB",
 };
-console.log(roles.designer);
+console.log(roles.designer); //KYB
+
+for (var name in roles) {
+  console.log("object => ", name, "value => ", roles[name]);
+}
