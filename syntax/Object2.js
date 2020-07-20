76d280a8d1055c1181d9a81a3c6fd2d1dfa55eb4
김현민 <hyunminy@gimhyeonmin-ui-MacBookAir.local>
2020-07-21 01:15:42 +0900

    객체-값으로서 함수

---
+++ syntax/Object2.js
@@ -0,0 +1,22 @@
+var f = function f1() {
+  console.log(1 + 1);
+  console.log(1 + 2);
+};
+console.log(f);
+f();
+
+//var i = if(true){console.log(1);}
+
+//var w = while(true)console.log(1);
+
+var f = function f1() {
+  console.log(1 + 1);
+  console.log(1 + 2);
+};
+var a = [f];
+a[0]();
+
+var o = {
+  func: f,
+};
+o.func();
